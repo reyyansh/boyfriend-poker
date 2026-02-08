5fe5faa73a6831183cf4eaa04f79edec5137f4d8
Reyansh Sathishkumar <sathishkumar@berkeley.edu>
2026-02-08 11:11:16 -0500

    trying username and login function

--- index.js
+++ index.js
@@ -8,15 +8,11 @@ const server = createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+  res.sendFile(join(__dirname, 'login.html'));
 });
 
 
 io.on('connection', (socket) => {
-  //console.log('a user connected');
-  //socket.on('disconnect', () => {
-    //console.log('user disconnected');
-  //});
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
